898725aaf660f9ed0c3d1a8d7c096e8583a38583
Sidhu102002 <sidharthgarhwal11@gmail.com>
2023-10-17 22:51:50 +0530

    last-commit-first-part

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,33 @@
 import AnimatedLetters from '../AnimatedLetters'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './index.scss'
 import Loader from 'react-loaders'
-import { useEffect,useState } from 'react'
+import { useEffect,useRef,useState } from 'react'
+import emailjs from'@emailjs/browser'
 const Contact =()=>{
     const[letterClass,setLetterClass]=useState('text-animate')
+    const refForm=useRef()
+
     useEffect(()=>{
         setTimeout(()=>{
            setLetterClass('text-animate-hover')
        },3000)
    },[])
+   const sendEmail=(e)=>{
+       e.preventDefault()
 
+       emailjs
+       .sendForm('service_jxofady','template_etr2nni',refForm.current,'okhI0yGMeWT-6aa8p')
+       .then(
+        ()=>{
+            alert('Message successfully sent!')
+            window.location.reload(false)
+        },
+        ()=>{
+            alert('Failed to send the message, please try again')
+        }
+       )
+   }
     return(
 
         <>
@@ -29,7 +47,7 @@ const Contact =()=>{
                 </p>
                 
                 <div className='contact-form'>
-                    <form>
+                    <form ref={refForm} onSubmit={sendEmail}>
                         <ul>
                             <li className='half'>
                                <input type='text' name="name" placeholder='Name' required/>
@@ -38,7 +56,7 @@ const Contact =()=>{
                                <input type='email' name="email" placeholder='email' required/>
                             </li>
                             <li>
-                                <input placeholder='subject' type='text' required/>
+                                <input placeholder='subject' name="subject" type='text' required/>
                             </li>
                             <li>
                                 <textarea placeholder='message' name='message' required></textarea>
@@ -49,6 +67,23 @@ const Contact =()=>{
                         </ul>
                     </form>
                 </div>
+            </div>
+            <div className='info-map'>
+               Sidharth,
+               <br/>
+               India,
+               <br/>
+               APJ Hostel, IIT Indore<br/>
+               Simrol<br/>
+               <span>sidharthgarhwal11@gmail.com</span>
+            </div>
+            <div className='map-wrap'>
+            <MapContainer center={[22.53213, 75.93460]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[22.53213, 75.93460]}>
+              <Popup>Sidharth lives here, come over for a cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer>
             </div>
         </div>
         <Loader type="pacman"/>
