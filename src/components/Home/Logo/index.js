4deb3157fcecf7b3ed10a4176616fccab7338b40
Sidhu102002 <sidharthgarhwal11@gmail.com>
2023-10-18 19:37:13 +0530

    GSAP_changed

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,8 +1,8 @@
 import './index.scss'
 import LogoS from '../../../assets/images/logo-s.png'
-import {DrawSVGPlugin} from 'gsap-trial/DrawSVGPlugin'
+import {DrawSVGPlugin} from 'gsap/DrawSVGPlugin'
 import { useEffect,useRef } from 'react'
-import gsap from 'gsap-trial'
+import gsap from 'gsap'
 
 const Logo=()=>{
 
